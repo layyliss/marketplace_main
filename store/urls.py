860ff75a145f64cdc5cd175bf5f8e0e93c51bc8b
unknown <layjaysalaco@gmail.com>
2025-11-11 12:06:05 -0800

    agregue login y registro

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import contact,detail
+from django.contrib.auth import views as auth_views
+from .views import contact, detail, register
 
-urlpatterns= [
-    path('contact/',contact,name='contact'),
-    path('detail/<int:pk>/',detail,name='detail'),
+from .forms import LoginForm
+
+urlpatterns = [
+    path('contact/', contact, name='contact'),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html', authentication_form=LoginForm)),
+    path('detail/<int:pk>/', detail, name='detail'),
 ]
