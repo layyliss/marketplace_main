4ec1163adec0fd4a4e1581806ecd89c39a3f9d31
Lay S <layjaysalaco@gmail.com>
2025-11-17 16:29:26 -0800

    agregué navegación de registro

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import contact, detail, register
+from .views import contact, detail, register, logout_user
 
 from .forms import LoginForm
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='store/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', logout_user, name='logout'),
     path('detail/<int:pk>/', detail, name='detail'),
 ]
